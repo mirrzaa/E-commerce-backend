6483e4e5760215094278721a5b5b42743a752c7a
mirrzaa <mirzashomol.karshiev@stud.uni-bamberg.de>
2023-03-23 17:06:55 +0100

    initial project structure

---
+++ src/main/java/com/example/eccomerbackend/models/entities/User.java
@@ -0,0 +1,16 @@
+package com.example.eccomerbackend.models.entities;
+
+import java.time.LocalDate;
+
+public class User {
+    private Long id;
+    private String firtsName;
+    private String lastName;
+    private String username;
+    private String password;
+    private LocalDate birthDate;
+    private String phoneNumber;
+    private String email;
+    private String postAddress;
+
+}
