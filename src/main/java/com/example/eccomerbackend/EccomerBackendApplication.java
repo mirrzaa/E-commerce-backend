cf9b86ef99cf37d7b38bd1091326db26c97e6bf3
Mirzashomol Karshiev <mirzashomol.karshiev@stud.uni-bamberg.de>
2023-03-23 16:56:32 +0100

    initial project structure

---
+++ src/main/java/com/example/eccomerbackend/EccomerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.eccomerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EccomerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EccomerBackendApplication.class, args);
+    }
+
+}
