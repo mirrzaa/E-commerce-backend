0133dad7b126347ba2c9072a18324734d255f25b
Suhrob <suhrobochilov15@gmail.com>
2023-03-26 15:19:07 +0200

    entity user

--- src/main/java/com/example/eccomerbackend/models/entities/User.java
+++ src/main/java/com/example/eccomerbackend/models/entities/User.java
@@ -1,23 +1,118 @@
 package com.example.eccomerbackend.models.entities;
 
+import jakarta.persistence.*;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @RequiredArgsConstructor
-@NoArgsConstructor
+//@NoArgsConstructor
+@Entity
+@Table(name = "user_table")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "firts_name", nullable= false)
     private String firtsName;
+    @Column(name = "last_name", nullable= false)
     private String lastName;
+    @Column(name = "user_name", nullable= false)
     private String username;
+    @Column(name = "password", nullable= false)
     private String password;
+    @Column(name = "birth_day", nullable= false)
     private LocalDate birthDate;
+    @Column(name = "phone_number")
     private String phoneNumber;
+    @Column(name = "email", nullable= false)
     private String email;
+    @Column(name = "post_adress", nullable= false)
     private String postAddress;
 
+    public User(Long id, String firtsName, String lastName, String username, String password, LocalDate birthDate, String phoneNumber, String email, String postAddress) {
+        this.id = id;
+        this.firtsName = firtsName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.birthDate = birthDate;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.postAddress = postAddress;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirtsName() {
+        return firtsName;
+    }
+
+    public void setFirtsName(String firtsName) {
+        this.firtsName = firtsName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPostAddress() {
+        return postAddress;
+    }
+
+    public void setPostAddress(String postAddress) {
+        this.postAddress = postAddress;
+    }
 }
