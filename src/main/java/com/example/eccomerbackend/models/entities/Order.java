ea64032bec3b55a7bcd73d7ef43255307275a55c
sanj94 <sanjar.haydarov1994@mail.ru>
2023-03-29 16:16:23 +0200

    entities Order and Product changed, not finished

--- src/main/java/com/example/eccomerbackend/models/entities/Order.java
+++ src/main/java/com/example/eccomerbackend/models/entities/Order.java
@@ -12,4 +12,43 @@ public class Order {
     private Long userId;
     private Date creationDate;
     private Date updatedDate;
+
+    public Order(Long id, Long userId, Date creationDate, Date updatedDate){
+        this.id = id;
+        this.userId = userId;
+        this.creationDate = creationDate;
+        this.updatedDate = updatedDate;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public Long getUserId(){
+        return userId;
+    }
+
+    public void setUserId(Long userId){
+        this.userId = userId;
+    }
+
+    public Date getCreationDate(){
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate){
+        this.creationDate = creationDate;
+    }
+
+    public Date getUpdatedDate(){
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDateDate){
+        this.updatedDate = updatedDateDate;
+    }
 }
--- src/main/java/com/example/eccomerbackend/models/entities/Product.java
+++ src/main/java/com/example/eccomerbackend/models/entities/Product.java
@@ -1,7 +1,11 @@
 package com.example.eccomerbackend.models.entities;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
+@Data
+@RequiredArgsConstructor
 @Entity
 public class Product {
     @Id
@@ -11,4 +15,52 @@ public class Product {
     private String description;
     private Long categoryId;
     private Integer price;
+
+    public Product(Long id, String name, String description, Long categoryId, Integer price){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.categoryId = categoryId;
+        this.price = price;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public Long getCategoryId(){
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId){
+        this.categoryId = categoryId;
+    }
+
+    public Integer getPrice(){
+        return price;
+    }
+
+    public void setPrice(Integer price){
+        this.price = price;
+    }
 }
