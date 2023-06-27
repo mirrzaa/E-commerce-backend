ba193c95a5d8a3f2284512bea1bc47f0ce5270d2
suhrobochilov <sukhrobochilov15@gmail.com>
2023-06-27 15:52:30 +0200

    created Configurations for ModelMapper

--- src/main/java/com/example/eccomerbackend/configurations/Configurations.java
+++ src/main/java/com/example/eccomerbackend/configurations/Configurations.java
@@ -1,4 +1,13 @@
 package com.example.eccomerbackend.configurations;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class Configurations {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
